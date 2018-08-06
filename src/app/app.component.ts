adfaee61f34eea303a26785e75e89419ddefc64e
Prince <prince.chattri@adapty.com>
2018-08-06 17:43:36 +0530

    Angular Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-test';
+export class AppComponent implements OnInit {
+  title = 'app';
+  data: any = {};
+  order: any = {};
+
+  constructor(private http: HttpClient) {
+    this.http.get<UserResponse>('http://northwind.servicestack.net/query/customers.json').subscribe(data => {
+      console.log(data.Results);
+      this.data = data;
+    });
+
+    this.http.get<UserResponse>('http://northwind.servicestack.net/query/orders.json').subscribe(res => {
+      console.log(res.Results);
+      this.order = res;
+    });
+  }
+  key: string = 'name'; //set default
+  reverse: boolean = false;
+  sort(key) {
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+
+  //initializing p to one
+  p: number = 1;
+  ngOnInit(): void {
+
+  }
+}
+
+interface UserResponse {
+  Results: any;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, NgxPaginationModule, Ng2SearchPipeModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
